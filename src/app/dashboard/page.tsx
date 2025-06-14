28ff01a1bdae856c160b3ccb25a1f5c4143d1066
Wahyu Pratama <email_baru@contoh.com>
2025-06-15 01:58:37 +0700

    add update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-
+import {
+  UserIcon,
+  LockClosedIcon,
+  WrenchScrewdriverIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { RiHome9Fill } from 'react-icons/ri';
 const getCookie = (name: string): string | undefined => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
@@ -18,19 +24,30 @@ export default function DashboardPage() {
   }, []);
 
   return (
-    <div className='min-h-screen flex items-center justify-center '>
+    <div className='min-h-screen flex items-center justify-center'>
       <div className='bg-white border rounded-xl p-8 w-full max-w-md flex flex-col items-center space-y-6'>
-        <h1 className='text-xl font-bold text-gray-800 mb-2'>
-          Selamat Datang{' '}
-        </h1>
+        <WrenchScrewdriverIcon className='h-10 w-10 text-yellow-500 mb-2 animate-bounce' />
+        <div className='mt-6 flex items-center gap-2 bg-yellow-100 text-yellow-800 px-3 py-2 rounded-lg text-sm'>
+          <WrenchScrewdriverIcon className='h-5 w-5' />
+          Dashboard masih dalam tahap pengembangan.
+        </div>
         <div className='w-full space-y-3'>
           <div className='flex flex-col'>
-            <span className='text-gray-500 text-sm'>Email</span>
+            <span className='flex items-center text-gray-500 text-sm gap-1'>
+              <UserIcon className='h-4 w-4 text-blue-500' /> Email
+            </span>
             <span className='font-medium text-gray-800'>{email}</span>
           </div>
-          <div className='flex flex-col'>
-            <span className='text-gray-500 text-sm'>Password</span>
-            <span className='font-medium text-gray-800'>{password}</span>
+          <div className='flex flex-row items-center justify-between'>
+            <div className='flex flex-col'>
+              <span className='flex items-center text-gray-500 text-sm gap-1'>
+                <LockClosedIcon className='h-4 w-4 text-green-500' /> Password
+              </span>
+              <span className='font-medium text-gray-800'>{password}</span>
+            </div>
+            <Link href='/' className='ml-4'>
+              <RiHome9Fill className='h-6 w-6 text-green-800' />
+            </Link>
           </div>
         </div>
       </div>
