a6525437bed17b72e5f7baabd9d83ba45318f3fb
Wahyu Pratama <email_baru@contoh.com>
2025-06-19 01:13:43 +0700

    update

--- src/app/components/atom/rekber.tsx
+++ src/app/components/atom/rekber.tsx
@@ -8,9 +8,15 @@ interface RekberProps {
 
 const Rekber: React.FC<RekberProps> = ({ className }) => {
   return (
-    <div className="flex justify-center items-center space-x-2">
+    <div className={`flex justify-center items-center space-x-2 ${className}`}>
       <Link href='/'>
-      <Image src='/images/Logo 1.png' alt="Logo"  width={150}  height={0} style={{ height: 'auto' }} />
+        <Image
+          src='/images/Logo 1.png'
+          alt='Logo'
+          width={150}
+          height={0}
+          style={{ height: 'auto' }}
+        />
       </Link>
     </div>
   );
