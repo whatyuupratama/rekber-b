d5b04b0e757b3db472095f988eda92ff08072f97
Wahyu Pratama <email_baru@contoh.com>
2025-06-19 19:28:10 +0700

    puss al

--- src/app/components/start/start.tsx
+++ src/app/components/start/start.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import NavBar from '../slicings/navbar';
 import Hero from '../slicings/section';
-// import CTA from '../cta';
-import Fitur from '../slicings/fitur';
-import Works from '../slicings/works';
 const Start = () => {
   return (
-    <div> 
-      <Hero /> 
+    <div>
+      <Hero />
     </div>
   );
 };
